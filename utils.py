1bbf71a95892c5ae73be11de8ef3f8bbd76bd8f6
Jorge Sanchez-Cano <jorgescmsd@gmail.com>
2020-03-21 09:02:15 +0000

    save cpu copy of parameters.

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import pickle, os, time, math
 
 def save_checkpoint(model, path, valid_loss, args={}):
     if path:
-        to_save = {'params' : model.state_dict(), 'valid_loss': valid_loss,
+        to_save = {'params' : model.state_dict().cpu(), 'valid_loss': valid_loss,
                    'args': args}
         with open(path, 'wb') as f:
             pickle.dump(to_save, f)
