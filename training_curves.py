abba54d45dfeb330149f145806818699b2aedbd5
Jorge Sanchez-Cano <jorgescmsd@gmail.com>
2020-04-04 12:41:14 +0100

    improved the code

--- training_curves.py
+++ training_curves.py
@@ -11,14 +11,12 @@ parser.add_argument('--description')
 parser.add_argument('--description-only', action='store_true')
 parser.add_argument('--plot-name', type=str, default='plot.png')
 
-def plot_curves(df_dict, fname='plot.png', time=True):
-    """ if time = True plot wrt time, otherwise, plt wrt epoch """
-    index = 'time'
+def plot_curves(df_dict, fname='plot.png', x_axis='time'):
     column = 'perplexity'
     for name, df in df_dict.items():
-        plt.plot(df[index], df[column], label = name)
+        plt.plot(df[x_axis], df[column], label = name)
     plt.legend()
-    plt.xlabel('time (mins)')
+    plt.xlabel(x_axis)
     plt.ylabel(column)
     plt.savefig(fname)
 
@@ -36,12 +34,22 @@ def load_dfs(dirs, names, file='valid.csv'):
         dfs[name] = relative_time(pd.read_csv(filename))
     return dfs
 
+def check_dir(path):
+    test_file = 'valid.csv'
+    if os.path.isdir(path):
+        # check test file is in the directory
+        if test_file in os.listdir(path):
+            return True
+    return False
+
 def get_dirs(directory, names=[]):
     if len(names)==0:
         names = os.listdir(directory)
     dirs = []
     for name in names:
-        dirs.append(os.path.join(directory, name))
+        path = os.path.join(directory, name)
+        if check_dir(path):
+            dirs.append(path)
     return dirs, names
 
 def descriptions(dirs, file_to='descriptions.txt', filename='description.txt'):
@@ -69,4 +77,4 @@ if __name__=='__main__':
         descriptions(dirs, file_to=args.description)
     if not args.description_only:
         df_dict = load_dfs(dirs, names)
-        plot_curves(df_dict, args.plot_name)
+        plot_curves(df_dict, args.plot_name, 'epoch')
