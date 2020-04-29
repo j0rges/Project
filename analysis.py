4d37f16c07004bc56138e3bacb94071d0ede7b1d
Jorge Sanchez-Cano <jorgescmsd@gmail.com>
2020-04-29 12:07:45 +0100

    added documentation and fixed mistakes.

--- analysis.py
+++ analysis.py
@@ -1,11 +1,14 @@
 import pandas as pd
-import pickle
+import pickle, os
 from training_curves import get_dirs
 
 GOLD_PATH='num_agr/subj_agr_filtered.gold'
 RESULTS_FILE='num_agr_result.pkl'
 
 def load_results(dir, filename='num_agr_result.pkl'):
+    """ Extract the results of the number agreement task from the directory.
+        filename is the name of the file in which they were stored.
+    """
     path = os.path.join(dir, filename)
     with open(path, 'rb') as f:
         dict_ = pickle.load(f)
@@ -13,6 +16,10 @@ def load_results(dir, filename='num_agr_result.pkl'):
 
 
 def results_dataframe(dir):
+    """ Create a dataframe with information of each test sentence for the number
+        agreement task, and whether it was predicted right by each saved model
+        in dir with stored results.
+    """
     dirs, names = get_dirs(dir, test_file=RESULTS_FILE)
     # Load the information about the sentences as a dataframe
     gold = pd.read_csv(GOLD_PATH, delimiter='\t',
@@ -21,20 +28,27 @@ def results_dataframe(dir):
     results = [load_results(dir) for dir in dirs]
     # Add the results to the dataframe
     for name,result in zip(names,results):
-        gold['name'] = result
+        gold[name] = result
     return gold, names
 
 def num_attractors_df(gold_df, names):
-    new_df = gold_df.[names + ['attractors']].groupby('attractors')\
+    """ names = name of columns with results (abstractly represent a model).
+        gold_df = a dataframe with columns names and 'attractors'.
+        returns dataframe with attractors as index and the performance of each
+        model.
+    """
+    new_df = gold_df[names + ['attractors']].groupby('attractors')\
                     .aggregate(['sum','count'])
-    new_df.swaplevel(axis=-1)
-    indices = list(zip(['proportion']*7,names))
+    new_df = new_df.swaplevel(axis=1)
+    indices = list(zip(['proportion']*len(names),names))
     # Get the proportion of correct answers for each number of attractors.
     new_df[indices] = new_df['sum']/new_df['count']
-    return new_df.swaplevel(axis=-1)
+    return new_df.swaplevel(axis=1)
 
 proportion = lambda x: sum(x)/len(x)
+
 def performance_df(gold_df, names):
+    """ Return dataframe with overall performance for each of names """
     performances = [proportion(gold_df[name]) for name in names]
     new_df = pd.DataFrame(data=[performances],columns=names)
     return new_df
