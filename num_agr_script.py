81cdd14a705979780780bc7b531df0fa260b4ab9
Jorge Sanchez-Cano <jorgescmsd@gmail.com>
2020-05-12 13:37:16 +0100

    added nonce support

--- num_agr_script.py
+++ num_agr_script.py
@@ -7,6 +7,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('directory', type=str)
 parser.add_argument('--logs', type=str, nargs='+', default=[])
 parser.add_argument('--results-file', type=str, default='num_agr_result.pkl')
+parser.add_argument('--nonce', action='store_true',
+        help='if provided, indicates the dataset is')
 
 args = parser.parse_args()
 
@@ -19,8 +21,12 @@ class FakeArgs():
         self.gold_file = gold_file
         self.nonce = nonce
 
-arguments = FakeArgs('checkpoint.pkl','num_agr/subj_agr_filtered.text',
-                     'num_agr/subj_agr_filtered.gold', False)
+if args.nonce:
+    arguments = FakeArgs('checkpoint.pkl','num_agr/generated.text',
+                     'num_agr/generated.tab', True)
+else:
+    arguments = FakeArgs('checkpoint.pkl','num_agr/subj_agr_filtered.text',
+                         'num_agr/subj_agr_filtered.gold', False)
 for dir in dirs:
     arguments.checkpoint = os.path.join(dir,'checkpoint.pkl')
     path = os.path.join(dir,args.results_file)
